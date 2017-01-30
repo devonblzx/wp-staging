677734f61232cf388c4863c0e6b317bc4cee91a7
René Hermenau <admin@xsimulator.net>
2017-01-30 13:06:35 +0100

    add pro files

---
+++ views/view-sync-settings.php
@@ -0,0 +1,9 @@
+<?php defined( 'ABSPATH' ) or exit; 
+?>
+  	<div class="wpstg-tabs-wrapper">  
+    test
+        </div>
+    	<a href="#" class="wpstg-prev-step-link wpstg-link-btn button-primary"><?php _e('Back', 'wpstg'); ?></a>
+	<a href="#" id="wpstg-start-cloning" class="wpstg-next-step-link wpstg-link-btn button-primary" data-action="wpstg_cloning"><?php  echo wpstg_return_button_title();?></a>
+
+
